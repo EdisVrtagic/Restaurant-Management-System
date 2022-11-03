1cfda792159dfa18179645f3cea731323c4a9359
Edis Vrtagić <101829021+EdisVrtagic@users.noreply.github.com>
2022-11-03 19:53:07 +0100

    Add files via upload

---
+++ dbo.StaffTbl.data.sql
@@ -0,0 +1,4 @@
+SET IDENTITY_INSERT [dbo].[StaffTbl] ON
+INSERT INTO [dbo].[StaffTbl] ([Rb], [Zaposlenik], [Spol], [Lozinka], [Plata]) VALUES (100, N'Edis', N'Muško', N'12345', N'5000')
+INSERT INTO [dbo].[StaffTbl] ([Rb], [Zaposlenik], [Spol], [Lozinka], [Plata]) VALUES (101, N'Sakiba', N'Žensko', N'sakibica777', N'560')
+SET IDENTITY_INSERT [dbo].[StaffTbl] OFF
